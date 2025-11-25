dec3267562c9b9ebc7391cfa0507d975b3384b49
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2025-11-26 00:07:24 +0100

    [env] update seeder and main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from app import create_app
-from config import DEV
+from config import DEV, PROD
+import os
 
-app = create_app(config_name=DEV)
+app = create_app(config_name=PROD if os.getenv('FLASK_ENV') == 'prod' else DEV)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
