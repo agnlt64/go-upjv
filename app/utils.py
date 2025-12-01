b29895f42c108298dbbf086d208745c6ab7787df
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2025-12-01 12:49:15 +0100

    [auth] add server data validation

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,10 @@
 from flask import jsonify
+import re
+
+UPJV_ID_REGEX = r'^[a-zA-Z][0-9]{8}$'
+PASSWORD_REGEX = r'^(?=.*[a-zA-Z])(?!.*\s)(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$'
+EMAIL_REGEX = r'^[a-zA-Z]+\.[a-zA-Z]+@([a-zA-Z0-9-]+\.)?u-picardie\.fr$'
+PHONE_REGEX = r'^[0-9]{10}$'
 
 def error(message: str):
     return jsonify({
@@ -11,3 +17,15 @@ def success(message: str):
         'success': True,
         'message': message
     })
+
+def check_email(email: str):
+    return re.match(EMAIL_REGEX, email)
+
+def check_password(password: str):
+    return re.match(PASSWORD_REGEX, password)
+
+def check_upjv_id(upjv_id: str):
+    return re.match(UPJV_ID_REGEX, upjv_id)
+
+def check_phone(phone: str):
+    return re.match(PHONE_REGEX, phone)
--- app/views/api.py
+++ app/views/api.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, request, redirect, url_for
+from flask import Blueprint, request, redirect, url_for, flash, render_template
 from flask_login import login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from app.models import User
-from app.utils import error, success
+from app.utils import error, success, check_email, check_password, check_upjv_id, check_phone
 from app import db
 
 api = Blueprint('api', __name__)
@@ -30,6 +30,10 @@ def toggle_admin_role(user_id):
 def login():
     upjv_id = request.form.get('id_upjv')
     password = request.form.get('password')
+
+    if not check_upjv_id(upjv_id):
+        flash('Invalid UPJV ID', 'error')
+        return render_template('login.html', upjv_id=upjv_id)
     
     user = User.query.filter_by(upjv_id=upjv_id).first()
     if user:
@@ -37,12 +41,13 @@ def login():
             login_user(user)
             return redirect(url_for('main.user_profile'))
         else:
-            return error('Invalid password')
-    return error('User not found')
+            flash('Invalid password', 'error')
+            return render_template('login.html', upjv_id=upjv_id)
+    flash('User not found', 'error')
+    return render_template('login.html', upjv_id=upjv_id)
 
 @api.route('/sign-up', methods=['POST'])
 def sign_up():
-    # todo: validate inputs
     last_name = request.form.get('nom')
     first_name = request.form.get('prenom')
     tel = request.form.get('tel')
@@ -51,12 +56,27 @@ def sign_up():
     password = request.form.get('password')
     confirm = request.form.get('confirm_password')
 
+    if not check_phone(tel):
+        flash('Invalid phone number', 'error')
+        return render_template('sign_up.html', last_name=last_name, first_name=first_name, upjv_id=upjv_id, email=email)
+    if not check_upjv_id(upjv_id):
+        flash('Invalid UPJV ID', 'error')
+        return render_template('sign_up.html', last_name=last_name, first_name=first_name, tel=tel, email=email)
+    if not check_email(email):
+        flash('Invalid email', 'error')
+        return render_template('sign_up.html', last_name=last_name, first_name=first_name, tel=tel, upjv_id=upjv_id)
+    if not check_password(password):
+        flash('Invalid password', 'error')
+        return render_template('sign_up.html', last_name=last_name, first_name=first_name, tel=tel, upjv_id=upjv_id, email=email)
+
     user = User.query.filter_by(upjv_id=upjv_id).first()
     if user:
-        return error('User already exists')
+        flash('User already exists', 'error')
+        return render_template('sign_up.html', last_name=last_name, first_name=first_name, tel=tel, email=email)
 
     if password != confirm:
-        return error('Passwords do not match')
+        flash('Passwords do not match', 'error')
+        return render_template('sign_up.html', last_name=last_name, first_name=first_name, upjv_id=upjv_id, tel=tel, email=email)
 
     user = User(last_name=last_name,
                 first_name=first_name,
